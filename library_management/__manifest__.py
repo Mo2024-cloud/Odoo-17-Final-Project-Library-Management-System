8d11aff16efe529ebe8c183ae63247d2adebd12e
Mo2024-cloud <mrfreelancer.org@gmail.com>
2025-06-29 20:10:37 +0300

    Add security.xml to manifest file

--- library_management/__manifest__.py
+++ library_management/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '17.0.1',
     'depends': ['base','mail'],
     'data': [
-        # 'security/security.xml',
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/base_menu.xml',
         'views/library_book_view.xml',
